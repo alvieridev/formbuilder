c0541aad4679dabac9a874afc8b7b80c8c2f100a
alvieridev <shitt7069@gmail.com>
2024-07-28 15:52:47 +0100

    postgress running locally

---
+++ prisma/seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient()
+
+
+async function main() {
+    const user = await prisma.user.upsert({
+        where: {  email: "alvierime@gmail.com"},
+        update: {},
+        create: {
+            name: "Alvieri",
+            email: "alvierime@gmail.com",
+        },
+    })
+    console.log({user})
+}
+
+main()
+    .then(() => prisma.$disconnect())
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect();
+        process.exit(1);
+    })
